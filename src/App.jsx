eb719e3b0f80f5d667add0785fb9cb4c73de526e
alfianahar <alfian.aswinda@gmail.com>
2022-08-01 17:14:24 +0700

    add id button for fcc test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -10,26 +9,28 @@ function App() {
         <div className="current"></div>
       </div>
       <div>
-        <button className="span-two">AC</button>
-        <button>/</button>
-        <button>*</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+        <button id="clear" className="span-two">
+          AC
+        </button>
+        <button id="divide">/</button>
+        <button id="multiply">*</button>
+        <button id="seven">7</button>
+        <button id="eight">8</button>
+        <button id="nine">9</button>
+        <button id="add">+</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="subtract">-</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="zero">0</button>
+        <button id="decimal">.</button>
+        <button id="equals">=</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
